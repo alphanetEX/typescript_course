5f419c3639c0d28159bdd70b53f2536f4f115728
AlphanetEX <fixtexhax@gmail.com>
2017-11-30 11:33:47 -0600

    completed example boolean expretions

--- class_1/app.js
+++ class_1/app.js
@@ -1,1 +1,11 @@
 console.log("hello world !!");
+// booleanos
+var charlie = false;
+var alphanet = false;
+var chichico = false;
+var variablex = true;
+charlie = alphabet();
+console.log(charlie);
+function alphabet() {
+    return true;
+}
--- class_1/app.ts
+++ class_1/app.ts
@@ -1,1 +1,16 @@
 console.log("hello world !!");
+
+// booleanos
+let charlie:boolean = false;
+let alphanet:boolean = false;
+let chichico:boolean = false;
+let variablex: boolean = true;
+
+
+charlie = alphabet();
+
+console.log(charlie); 
+
+function alphabet(){
+    return true; 
+}
