f86dfb0982ca739b8b05c1437e15c8bf5f7b3751
alphanetEX <fixtexhax@gmail.com>
2017-12-13 12:05:08 -0600

    added basis example how to works class, constructor abd objets in ts

--- class_6/app.js
+++ class_6/app.js
@@ -1,1 +1,25 @@
 console.log("hello class objets, constructors and methods");
+var general_check = /** @class */ (function () {
+    function general_check(pdu, battery, wimax, ssid, dbm, battery_voltage, flyght_status, current_flighs) {
+        //declare objet variable in the constructor variables 
+        this.pdu = pdu;
+        this.battery = battery;
+        this.wimax = wimax;
+        this.ssid = ssid;
+        this.dbm = dbm;
+        this.battery_voltage = battery_voltage;
+        this.flight_status = flyght_status;
+        this.current_flights = current_flighs;
+    }
+    return general_check;
+}());
+var pdu = true;
+var battery = true;
+var wimax = true;
+var ssid = true;
+var dbm = -34;
+var battery_voltage = 22.4;
+var flight_status = true;
+var current_flights = 54;
+var drone = new general_check(pdu, battery, wimax, ssid, dbm, battery_voltage, flight_status, current_flights); // equal java, the variable drone it is equal to class general check so that obtain 
+console.log(drone); //objet variables to general check 
--- class_6/app.ts
+++ class_6/app.ts
@@ -1,1 +1,47 @@
 console.log("hello class objets, constructors and methods"); 
+
+class general_check {
+ //objeto de variables 
+ pdu: boolean;
+ battery:boolean; 
+ wimax:boolean; 
+ ssid:boolean; 
+ dbm:number; 
+ battery_voltage:number
+
+ flight_status:boolean; 
+ current_flights:number; 
+
+ constructor(pdu:boolean, battery:boolean, wimax:boolean, ssid:boolean, dbm:number, 
+    battery_voltage:number, flyght_status:boolean, current_flighs:number){
+
+    //declare objet variable in the constructor variables 
+
+    this.pdu= pdu; 
+    this.battery = battery; 
+    this.wimax = wimax; 
+    this.ssid = ssid; 
+    this.dbm = dbm; 
+    this.battery_voltage  = battery_voltage; 
+    this.flight_status = flyght_status; 
+    this.current_flights = current_flighs; 
+ }
+} 
+
+
+
+let pdu: boolean = true;
+let battery:boolean = true; 
+let wimax:boolean = true; 
+let ssid:boolean = true; 
+let dbm:number = -34; 
+let battery_voltage:number = 22.4; 
+
+let flight_status:boolean = true; 
+let current_flights:number = 54; 
+
+
+let drone:general_check = new general_check( pdu, battery, wimax, ssid, dbm, battery_voltage, flight_status, current_flights); // equal java, the variable drone it is equal to class general check so that obtain 
+console.log (drone);                           //objet variables to general check 
+
+
